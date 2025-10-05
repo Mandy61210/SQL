b5d9f2494a125c526adf50379f740faab7c79bc9
Mandy61210 <mandy61210@gmail.com>
2025-10-05 10:08:20 +0200

    stringoperations.py

---
+++ stringoperations.py
@@ -0,0 +1,18 @@
+first_name = "Codingal"
+last_name = "Educations"
+full_name = first_name+last_name
+example= "haa"*5
+
+
+print("first name :", first_name)
+print("last name :", last_name)
+print("full name :", full_name)
+print("strings multiplied by 5 times gives you this result :", example)
+
+
+word = 'coding'
+print("length of string :", len(word))
+print("first letter of string :", word[0])
+print("last letter of string :", word[5])
+print("string sliced :", word[0:3])
+
