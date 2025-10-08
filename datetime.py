2cd3a0aa2b64d78d903993344602c9a33e906c53
Mandy61210 <mandy61210@gmail.com>
2025-10-08 16:04:46 +0200

    datetime.py

---
+++ datetime.py
@@ -0,0 +1,14 @@
+import datetime
+
+# using now() to get current time
+current_time = datetime.datetime.now()
+
+
+# printing value of now.
+print ("time now at greenwhich mederian is :", end = "")
+print (current_time)
+
+# print calendar of 2021
+import calendar
+print("\n", calendar.calendar(2021))
+
