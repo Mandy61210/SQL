43f156cac32cb7f713461ff80f5aeb1605b06e28
Mandy61210 <mandy61210@gmail.com>
2025-10-08 15:52:53 +0200

    bminumbercheck.py

---
+++ bminumbercheck.py
@@ -0,0 +1,16 @@
+num = int(input("enter number to check :"))
+
+if num>50:
+        print("number is greater than 50")
+        if num%2==0:
+               print("and it is even to")
+
+        else:
+                print("and it is odd")
+
+
+else:
+        print("number is less then 50")
+                
+                       
+
